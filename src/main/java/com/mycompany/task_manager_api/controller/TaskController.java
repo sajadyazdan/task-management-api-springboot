f0823f5ea101ada8e192f86362cdb8114c8da3bb
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-02-15 23:13:30 -0500

    Create Task Controller

---
+++ src/main/java/com/mycompany/task_manager_api/controller/TaskController.java
@@ -0,0 +1,45 @@
+package com.mycompany.task_manager_api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.mycompany.task_manager_api.model.Task;
+import com.mycompany.task_manager_api.service.TaskService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PostMapping
+    public Task createTask(@Valid @RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @Valid @RequestBody Task updatedTask) {
+        return taskService.updateTask(id, updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
